df77d27aa4c73e4649c9a0b3588b6525f71cb8fc
zjl <averey.coder@gmail.com>
2024-06-16 00:08:45 +0800

    add sp keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -18,6 +18,7 @@ end)
 
 keymap('v', '<', '<gv')
 keymap('v', '>', '>gv')
+keymap('n', 'sp', '`[v`]') -- [S]elect [P]aste
 keymap({ 'n', 'i' }, '<C-s>', '<ESC>:wa<CR>')
 
 -- keymap({ 'n', 'i' }, '<C-,>', '<ESC><cmd>vsp $MYVIMRC<CR><Esc>')
@@ -48,20 +49,6 @@ keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnosti
 keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 keymap('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 -- keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
--- keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- TIP: Disable arrow keys in normal mode
--- keymap('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- keymap('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- keymap('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- keymap('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
