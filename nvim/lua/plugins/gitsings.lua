0e2cec9d67b920331d450637677368fb3cf7d5f9
jinlin.zhou <13611464403@139.com>
2024-06-25 17:35:27 +0800

    setting

--- nvim/lua/plugins/gitsings.lua
+++ nvim/lua/plugins/gitsings.lua
@@ -24,6 +24,30 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
       --   -- changedelete = { text = '~' },
       --   -- untracked = { text = '┆' },
       -- },
+      -- current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      -- current_line_blame_opts = {
+      --   virt_text = true,
+      --   -- virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      --   virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+      --   delay = 200,
+      --   ignore_whitespace = false,
+      --   virt_text_priority = 100,
+      -- },
+      on_attach = function (bufnr)
+        local gitsigns = require('gitsigns')
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+        map('n', '<leader>gb', gitsigns.blame_line)
+        map('n', '<leader>gB', function() gitsigns.blame_line{full=true} end)
+        map('n', '<leader>gd', gitsigns.diffthis)
+        map('n', '<leader>gD', function() gitsigns.diffthis('~') end)
+      end
     }
   end,
 }
